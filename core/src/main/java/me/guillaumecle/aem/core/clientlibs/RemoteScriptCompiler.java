12fb4c99373f6bae27b482a8aadf8c837e405449
Guillaume Clement <guillaumecle.me@gmail.com>
2020-03-22 14:58:19 -0400

    Adding tests and error writer

--- core/src/main/java/me/guillaumecle/aem/core/clientlibs/RemoteScriptCompiler.java
+++ core/src/main/java/me/guillaumecle/aem/core/clientlibs/RemoteScriptCompiler.java
@@ -7,6 +7,14 @@ import com.adobe.granite.ui.clientlibs.script.CompilerContext;
 import com.adobe.granite.ui.clientlibs.script.ScriptCompiler;
 import com.adobe.granite.ui.clientlibs.script.ScriptResource;
 
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,18 +33,16 @@ public class RemoteScriptCompiler implements ScriptCompiler {
             throws IOException {
 
         if (scriptResources != null && !scriptResources.isEmpty()) {
-            scriptResources.stream()
-                .peek(first -> setReturnContext(first.getName()))
-                .forEach(resource -> {
-                    try {
-                        out.write("/* Start: RemoteScriptCompiler Library */");
-                        out.write("ScriptResource Name: " + resource.getName());
-                        out.write("Compiler Context: " + ctx.getDestinationPath());
-                        out.write("/* End: RemoteScriptCompiler Library */");
-                    } catch (IOException e) {
-                        log.error("", e);
-                    }
-                });
+            scriptResources.stream().peek(first -> setReturnContext(first.getName())).forEach(resource -> {
+                try {
+                    out.write("/* Start: RemoteScriptCompiler Library */");
+                    out.write("ScriptResource Name: " + resource.getName());
+                    out.write("Compiler Context: " + ctx.getDestinationPath());
+                    out.write("/* End: RemoteScriptCompiler Library */");
+                } catch (final IOException e) {
+                    writeError(out, resource.getName(), "Failed to compile library" + e.getMessage());
+                }
+            });
         }
     }
 
@@ -61,23 +67,63 @@ public class RemoteScriptCompiler implements ScriptCompiler {
     }
 
     /**
-     * A method to set return mime type and extension based on the libraries
-     * being complied (JS/CSS).
+     * A method to set return mime type and extension based on the libraries being
+     * complied (JS/CSS).
      * 
      * @param resourceName
      */
     void setReturnContext(final String resourceName) {
-        if(resourceName.endsWith(".css." + COMPILER_EXTENSION)){
+        if (resourceName.endsWith(".css." + COMPILER_EXTENSION)) {
             MIME_TYPE = "text/css";
             OUTPUT_EXTENSION = "css";
-        }
-        else if(resourceName.endsWith(".js." + COMPILER_EXTENSION)){
+        } else if (resourceName.endsWith(".js." + COMPILER_EXTENSION)) {
             MIME_TYPE = "application/javascript";
             OUTPUT_EXTENSION = "js";
-        }
-        else{
+        } else {
             MIME_TYPE = "text/plain";
             OUTPUT_EXTENSION = "txt";
         }
     }
+
+    String getRemoteLibrary(final String libUrl) throws IOException {
+        final HttpGet request = new HttpGet(libUrl);
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(request)) {
+
+            final int responseCode = response.getStatusLine().getStatusCode();
+
+            if (responseCode == HttpStatus.SC_OK){
+                final HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return as a String
+                    return EntityUtils.toString(entity);
+                }
+                else{
+                    return "/* Empty Library Returned: [" + libUrl + "]*/";
+                }
+            }
+            else {
+                throw new HttpException("Invalid response code received [" + responseCode + "] from remote library URL [" + libUrl + "]");
+            }
+        }
+    }
+
+    void writeError(final Writer out, final String name, final String message) {
+
+        final String n = System.lineSeparator();
+
+        log.error("Failed to fetch or compile remote library {}: {}", name, message);
+
+        try {
+            out.write("/*****************************************************" + n);
+            out.write("Fetching or compilation resulted in an error" + n);
+            out.write("Input: " + name + n);
+            out.write("Error: " + message + n);
+            out.write("*****************************************************/" + n);
+        } catch (final IOException e) {
+            log.error("Failed to append error message to writer", e);
+        }
+    }
+
 }
--- core/src/test/java/me/guillaumecle/aem/core/clientlibs/RemoteScriptCompilerTest.java
+++ core/src/test/java/me/guillaumecle/aem/core/clientlibs/RemoteScriptCompilerTest.java
@@ -2,11 +2,17 @@ package me.guillaumecle.aem.core.clientlibs;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class RemoteScriptCompilerTest{
+class RemoteScriptCompilerTest {
 
     private RemoteScriptCompiler fixture;
 
@@ -16,23 +22,43 @@ class RemoteScriptCompilerTest{
     }
 
     @Test
-    void testSetReturnContextJS(){
+    void testSetReturnContextJS() {
         fixture.setReturnContext("hello.js.href");
+        assertAll(() -> assertEquals("application/javascript", fixture.getMimeType(), "Test JS Mime Type"),
+                () -> assertEquals("js", fixture.getOutputExtension(), "Test JS Output Extension"));
+    }
+
+    @Test
+    void testSetReturnContextCSS() {
+        fixture.setReturnContext("hello.css.href");
+        assertAll(() -> assertEquals("text/css", fixture.getMimeType(), "Test CSS Mime Type"),
+                () -> assertEquals("css", fixture.getOutputExtension(), "Test CSS Output Extension"));
+    }
+
+    @Test
+    void testGetRemoteLibrary() throws IOException {
+        String script = fixture.getRemoteLibrary("https://code.jquery.com/jquery-3.4.1.slim.min.js");
         assertAll(
-            () -> assertEquals("application/javascript", fixture.getMimeType(), "Test JS Mime Type"),
-            () -> assertEquals("js", fixture.getOutputExtension(), "Test JS Output Extension")
+            () -> assertNotNull(script),
+            () -> assertTrue(script.contains("jQuery v3.4.1"), "Test contents of returned script")
         );
     }
 
     @Test
-    void testSetReturnContextCSS(){
-        fixture.setReturnContext("hello.css.href");
-        assertAll(
-            () -> assertEquals("text/css", fixture.getMimeType(), "Test CSS Mime Type"),
-            () -> assertEquals("css", fixture.getOutputExtension(), "Test CSS Output Extension")
-        );
+    void testGetRemoteLibrary404() throws IOException {
+        assertThrows(IOException.class, () -> {
+            fixture.getRemoteLibrary("https://code.jquery.com/jquery-3.4.1.slim.min.js.404");
+        });
     }
 
-
-
+    @Test
+    void testWriteError() throws IOException {
+        StringWriter out = new StringWriter();
+        fixture.writeError(out, "hello.js", "Failed to fetch library");
+        assertAll(
+            () -> assertNotNull(out),
+            () -> assertTrue(out.toString().contains("Input: hello.js"), "Test contents of error writer"),
+            () -> assertTrue(out.toString().contains("Error: Failed to fetch library"), "Test contents of error writer")
+        );
+    }
 }
